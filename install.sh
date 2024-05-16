ede97677716d7b6982595b7b542955f80948aec5
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-05-16 00:39:37 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate https://github.com/clangcn/ngrok-one-key-install/raw/master/install_ngrok.sh -O ./install_ngrok.sh
+chmod 500 ./install_ngrok.sh
+./install_ngrok.sh install	
