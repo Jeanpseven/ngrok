009d78a6a9412e889482741175fdbce7877b6ac5
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-05-16 00:46:18 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-wget --no-check-certificate https://github.com/clangcn/ngrok-one-key-install/raw/master/install_ngrok.sh -O ./install_ngrok.sh
-chmod 500 ./install_ngrok.sh
-./install_ngrok.sh install	
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
